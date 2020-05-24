190225fb697ce2a4a799ebee80c2ba4123545886
MichellSara <65839095+MichellSara@users.noreply.github.com>
2020-05-23 23:11:16 -0500

    Add files via upload

---
+++ Personal.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<Title> Personal </Title>
+</head>
+<body 
+background="11.jpg">
+
+
+<font face="Courier New" color="white"> <h1>El lavado de vehículos hoy en día se ha convertido en una experiencia de limpieza, comodidad y confortabilidad. Nos enfocamos en brindarle esta experiencia con los servicios que Ud. necesita.
+Nuestros procesos, insumos, mano de obra, equipos e infraestructura están alineados para crear esta experiencia</h1></font>
+
+         <img src="5.jpg" width="500" height="400">            <img src="3.jpg" width="500" height="400">
+<h1> Ubicacion y Horarios </h1>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d243.8087241214325!2d-77.0410289928266!3d-12.116286199463811!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c83ba398268f%3A0xd85c6ab264667021!2sHouse%20Car%20Wash!5e0!3m2!1ses-419!2smx!4v1588476673782!5m2!1ses-419!2smx" 
+width="500" height="400" frameborder="2" style="border:2;" allowfullscreen="" aria.hidden="false" tabindex="0"></iframe>
+<font face="Courier New" color="white"> <h1>De Lunes a Sabado de 8:00 am a 6:00 pm</h1>
+</body>
+</html>
---
+++ Servicios.php
@@ -0,0 +1,87 @@
+<html>
+<head>
+<Title> Servicios </Title>
+</head>
+<body 
+background="10.jpg">
+<br>
+<center> 
+<img src="8.jpg" width="500" height="500">
+</center> 
+
+<ul type="disc">
+<li><font face="Consolas" size="6" color="Black"> <h1>Especial $80.00</h1></font>
+<ul type="square">
+<li>Aplicación de Protectores
+<li>De Pintura
+<li>De Motor
+<li>De Asientos
+<li> De Alfombras
+<li>De Cueros
+<li>De Salon
+<li>Undercoating
+<li>Economico
+<li>Especial
+<li>Arenado
+<li>Teñido de Cueros
+<li>Tapizados
+<li>Aire Acondicionado
+<li>Diagnostico
+<li>Carga
+<li>Limpieza y Desinfectado
+</li>
+</ul>
+
+<ul type="disc">
+<li><font face="Consolas" size="6" color="Black"> <h1>Lavado Expreso $85.00</h1></font>
+<ul type="square">
+<li>Aspirado de Asientos
+<li>Aspirado de Alfombras
+<li>Limpieza de Puertas y Tablero
+<li>Aplicación de Silicona al Tablero
+<li>Lavado de Carrocería
+<li>Aplicación de Silicona especial a las Llantas
+<li>Incluyendo encerado
+</li>
+</ul>
+
+<ul type="disc">
+<li><font face="Consolas" size="6" color="Black"> <h1>Lavado Economico $90.00</h1></font>
+<ul type="square">
+<li>Aspirado de Asientos
+<li>Aspirado de Alfombras
+<li>Aspirado de Maletera
+<li>Limpieza de Puertas y Tablero
+<li>Aplicación de Silicona a todo el Interior
+<li>Lavado de Chasis (parte baja del vehículo)
+<li>Lavado de Motor
+<li>Aplicación de Silicona Especial al Motor
+<li>Lavado de Carrocería
+<li>Aplicación de Silicona especial a las Llantas
+<li>Perfumado de Salón
+<li>Incluyendo encerado
+</li>
+</ul>
+
+<ul type="disc">
+<li><font face="Consolas" size="6" color="Black"> <h1>Lavado Ejecutivo $95.00</h1></font>
+<ul type="square">
+<li>Aspirado de Asientos
+<li>Aspirado de Alfombras
+<li>Lavado de Puertas
+<li>Lavado de Techo
+<li>Lavado de Cinturones de Seguridad
+<li>Lavado de Tablero, Timón y Consola
+<li>Lavado de Maletera
+<li>Aplicación de Silicona a todo el Interior
+<li>Lavado de Chasis (parte baja del vehículo)
+<li>Lavado de Motor
+<li>Aplicación de Silicona Especial al Motor
+<li>Lavado de Carrocería
+<li>Lavado de Bisagras, Jebes de las Puertas y Jebes de las Ventanas
+<li>Aplicación de Silicona a todos los Jebes
+<li>Aplicación de Silicona especial a las Llantas
+<li>Encerado de Carrocería
+<li>Perfumado de Salón
+</li>
+</ul>
