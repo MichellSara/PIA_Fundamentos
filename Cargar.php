c3172ac7f1edf97469836667653cb42776a61ddb
MichellSara <65839095+MichellSara@users.noreply.github.com>
2020-05-23 23:17:12 -0500

    Add files via upload

---
+++ Cargar.php
@@ -0,0 +1,10 @@
+<?php
+$nombre=$_GET['nombre'];
+$asunto=$_GET['asunto'];
+$email=$_GET['email'];
+
+echo"<h2> Gracias por escribirnos </h2>";
+echo "Se le envio informacion a este correo: " . $email . "<br/>";
+
+?>
+<a href="https://michellsara.github.io/PIA_Fundamentos/">Regresar</a>
---
+++ Form.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<Title> Formulario </Title>
+<meta charset="utf8">
+</head>
+<body bgcolor="turquoise">
+<h1> <center> Escribenos </center></h1>
+
+<!--FORMULARIO-->
+<font face="Arial Black" size="6"><form action="cargar.php" method="get">
+<p>Nombre:<input type="text" name="nombre" /></p>
+<p>Asunto:<input type="text" name="asunto" /></p>
+<p>Email:<input type="text" name="email" /></p>
+<p><input type="submit" name="Enviar" vale="Enviar" /></p>
+
+<a herf="https://michellsara.github.io/PIA_Fundamentos/">Regresar</a></center></h1>
+</body>
+</html>
